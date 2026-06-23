a1828d6ed46c0a981d30cc91a18f2ea271a2cd66
tmdl2714 <tiemadelong@gmail.com>
2026-06-23 16:17:40 +0800

    Add posters for video category works

--- category-data.js
+++ category-data.js
@@ -220,9 +220,9 @@ const CATEGORY_DATA = {
       { type: "video", src: "./assets/categories/video/video-01.mp4", title: "窖藏酒CG动画", orientation: "video-landscape" },
       { type: "video", src: "./assets/categories/video/video-02.mp4", title: "白酒广告短片", orientation: "video-landscape" },
       { type: "video", src: "./assets/categories/video/video-03.mp4", title: "护肤品精华水广告", orientation: "video-landscape" },
-      { type: "video", src: "./废土温情_25MB版.mp4", title: "废土温情", orientation: "video-landscape" },
+      { type: "video", src: "./废土温情_25MB版.mp4", title: "废土温情", orientation: "video-landscape", poster: "./废土温情.png" },
       { type: "video", src: "./猫粮广告_25MB版.mp4", title: "猫粮广告", orientation: "video-landscape" },
-      { type: "video", src: "./耳机广告_25MB版.mp4", title: "耳机广告", orientation: "video-landscape" },
+      { type: "video", src: "./耳机广告_25MB版.mp4", title: "耳机广告", orientation: "video-landscape", poster: "./小耳机.png" },
       { type: "video", src: "./渡鸦的礼物.mp4", title: "渡鸦的礼物", orientation: "video-landscape", poster: "./渡鸦的礼物.png" },
       { type: "video", src: "./山海镖局_GitHub_25MB内.mp4", title: "山海镖局", orientation: "video-landscape", poster: "./山海镖局.png" }
     ],
