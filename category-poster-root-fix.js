8d0e726a8d88f5c28a6ed8a809bfc51c8f1fab1e
tmdl2714 <tiemadelong@gmail.com>
2026-06-24 00:21:22 +0800

    Add poster root data cleanup before render

---
+++ category-poster-root-fix.js
@@ -0,0 +1,37 @@
+(() => {
+  const data = window.CATEGORY_DATA;
+  if (!data?.poster) return;
+
+  data.poster.works = [
+    { type: "image", src: "./ac6fe018-f7f3-4ffa-b677-e88fb228d2eb.png", title: "海报 Banner 01", orientation: "banner" },
+    { type: "image", src: "./d15079c8-14f0-47b8-864b-dd47c04d6d66.png", title: "海报 Banner 02", orientation: "banner" },
+    { type: "image", src: "./30cb955e-0822-43f3-98d9-4ba484c120d2.png", title: "海报 Banner 03", orientation: "banner" },
+    { type: "image", src: "./4cca3669-f0f0-4b6e-8557-176422ad07b2.png", title: "海报 Banner 04", orientation: "banner" },
+    { type: "image", src: "./01_拼车计划.png", title: "拼车计划", orientation: "activity-poster" },
+    { type: "image", src: "./02_周末狂欢夜.png", title: "周末狂欢夜", orientation: "activity-poster" },
+    { type: "image", src: "./03_城限专场.png", title: "城限专场", orientation: "activity-poster" },
+    { type: "image", src: "./04_DM严选.png", title: "DM严选", orientation: "activity-poster" },
+    { type: "image", src: "./05_周年庆典.png", title: "周年庆典", orientation: "activity-poster" },
+    { type: "image", src: "./06_未知剧场.png", title: "未知剧场", orientation: "activity-poster" },
+    { type: "image", src: "./e607c69f-7930-43c3-9774-86b421ee205b (1).png", title: "竖版海报 01", orientation: "vertical" },
+    { type: "image", src: "./d2977236-d08b-41b3-b595-7e07d9005e6f.png", title: "竖版海报 02", orientation: "vertical" },
+    { type: "image", src: "./b6a25755-6b4e-4a4a-9d1c-54a714fd7af5.png", title: "竖版海报 03", orientation: "vertical" },
+    { type: "image", src: "./hp耳机2.png", title: "竖版海报 04", orientation: "vertical" },
+    { type: "image", src: "./小水库8.png", title: "竖版海报 05", orientation: "vertical" },
+    { type: "image", src: "./小水库10.png", title: "竖版海报 06", orientation: "vertical" },
+    { type: "image", src: "./hp耳机4.1.png", title: "竖版海报 07", orientation: "vertical" },
+    { type: "image", src: "./b64cc7fb-e4b1-4c98-8b1c-e6c5c0eaea00.png", title: "竖版海报 08", orientation: "vertical" },
+    { type: "image", src: "./3b157685-11e4-44e0-b277-f60f370a452a.png", title: "竖版海报 09", orientation: "vertical" },
+    { type: "image", src: "./1e8a75ec-0c7a-4906-8e73-032495bc0edf (1).png", title: "竖版海报 10", orientation: "vertical" },
+    { type: "image", src: "./0ef45b3b-e083-49ce-a8f5-1a270e8b5662.png", title: "竖版海报 11", orientation: "vertical" },
+    { type: "image", src: "./6e143708-8fcf-43d4-a238-808aaf208d1d.png", title: "竖版海报 12", orientation: "vertical" },
+    { type: "image", src: "./b3b353e8-14ec-4ee0-a6a1-996110964114.png", title: "竖版海报 13", orientation: "vertical" },
+    { type: "image", src: "./904f694f-8f4d-438f-bd72-7c43f7b049fd.png", title: "竖版海报 14", orientation: "vertical" },
+    { type: "image", src: "./85a1d84b-234c-4be8-86c2-496943bfdc4c.png", title: "竖版海报 15", orientation: "vertical" },
+    { type: "image", src: "./hp耳机10.1.png", title: "竖版海报 17", orientation: "vertical" },
+    { type: "image", src: "./hp耳机7.png", title: "竖版海报 18", orientation: "vertical" },
+    { type: "image", src: "./小水库5.png", title: "竖版海报 19", orientation: "vertical" },
+    { type: "image", src: "./制作海报 (2).png", title: "竖版海报 20", orientation: "vertical" },
+    { type: "image", src: "./制作海报 (4).png", title: "竖版海报 21", orientation: "vertical" }
+  ];
+})();
